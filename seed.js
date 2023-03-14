0e9cf776b63e63cb45e95a6665907a35c52d75b3
Ryan Bagan <darcknight1980@gmail.com>
2023-03-14 13:34:12 -0700

    made changes

--- seed.js
+++ seed.js
@@ -22,10 +22,26 @@ let aBook = new Schema ({
 await Books.create({
   title: 'The Dark Knight',
   description: 'Justice and Vengeance! Hero that we deserve!',
-  status: 'Out and about breaking bones and spirit!'
+  status: 'Out and about at night breaking bones and spirit!'
 });
 
 console.log('Batman is The Dark Knight!');
+
+await Books.create({
+  title: 'S is for Hope',
+  description: 'A God among men.',
+  status: 'Clark Kent as man, Superman as a God.'
+});
+
+console.log('Superman stands for Hope.');
+
+await Books.create({
+  title: 'Killing Joke',
+  description: 'Joker causing death and chaos!',
+  status: 'He is the most dangerous enemy of Batman.'
+});
+
+console.log('Why so serious?');
 mongoose.disconnect();
 }
 
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const Books = require('./selection/books.js');
 
 // bring in mongoose
 const mongoose = require('mongoose');
+const { response } = require('express');
 
 // add validation to confirm we are wire up to our mongo DB
 const db = mongoose.connection;
@@ -28,10 +29,36 @@ app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+//app.get('/test', (request, response) => {
 
-  response.send('test request received')
+//  response.send('test request received')
 
-})
+//});
 
+// ROUTES
+app.get('/', (request, response) => {
+  response.status(200).send('Welcome to Gotham City Library!');
+});
+
+app.get('/books', getBooks);
+async function getBooks(request, response, next) {
+  try {
+    let results = await Books.find({});
+    res.status(200).send(results);
+
+  } catch(error) {
+    next(error);
+  }
+}
+
+app.get('*', (request, response) => {
+  response.status(404).send('Not available');
+});
+
+// ERROR
+app.use((error, request, response, next) => {
+  response.status(500).send(error.message);
+});
+
+// LISTEN
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
