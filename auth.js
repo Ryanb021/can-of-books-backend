d36286cceca794b8cd116cbf25fabdc02afd5867
Ryan Bagan <darcknight1980@gmail.com>
2023-03-17 12:40:55 -0700

    made changes

---
+++ auth.js
@@ -0,0 +1,8 @@
+'use strict'
+
+// jwt - JSON Web Token
+// install with: npm i jsonwebtoken
+const jwt = require('jsonwebtoken');
+// jwks - JSON Web Key Set
+// install with: npm i jwks-rsa
+const jwksClient = require('jwks-rsa');
